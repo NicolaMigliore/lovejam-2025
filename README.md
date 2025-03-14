600bb14f3b9e5e2018c92ff24fa862eb2dce37e9
NicolaMigliore <38867373+NicolaMigliore@users.noreply.github.com>
2025-03-14 12:21:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# love2d-template
+Template repository for game development using LÖVE
---
+++ globals.lua
@@ -0,0 +1,9 @@
+
+-- Load libraries
+Camera = require 'libs.hump.camera'
+GameState = require 'libs.hump.gamestate'
+Vector = require 'libs.hump.vector'
+Object = require 'libs.classic'
+Husl = require 'libs.husl'
+Inspect = require 'libs.inspect'
+Lume = require 'libs.lume'
